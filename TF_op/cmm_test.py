b0106003121c3bc80b7e532ec29431fac5a39a4b
Jack Weissenberger <johnaweissenberger@gmail.com>
2020-04-13 18:24:09 -0400

    custom core test

--- TF_op/cmm_test.py
+++ TF_op/cmm_test.py
@@ -8,19 +8,24 @@ import unittest
 #classic_mm_module = tf.load_op_library('./classic_mat_mul.so')
 fast_mm_module = tf.load_op_library('./fast_mat_mul.so')
 
-tf.random.set_seed(1)
+#tf.random.set_seed(1)
 
-a = tf.Variable(tf.random.uniform(shape=(4, 5)))
-b = tf.Variable(tf.random.uniform(shape=(5, 6)))
+
+
+a = tf.Variable(tf.random.uniform(shape=(800, 800)))
+b = tf.Variable(tf.random.uniform(shape=(800, 800)))
 
 #op = classic_mm_module.ClassicMatMul(a_matrix=a, b_matrix=b)
-op = fast_mm_module.FastMatMul(a_matrix=a, b_matrix=b)
 
-regular = tf.matmul(a, b)
+print("looping:")
+for i in range(100):
+    op = fast_mm_module.FastMatMul(a_matrix=a, b_matrix=b)
 
-print('\n\n\nregular: ', regular)
-print('\n\n\nop: ', op)
-print('\n\n\nop-regular ', op-regular)
+# regular = tf.matmul(a, b)
+# 
+# print('\n\n\nregular: ', regular)
+# print('\n\n\nop: ', op)
+# print('\n\n\nop-regular ', op-regular)
 
 
 
