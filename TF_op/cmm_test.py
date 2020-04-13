a32399152678e3cfda88b21d74c0f6cd4d5a4da5
Jack Weissenberger <johnaweissenberger@gmail.com>
2020-04-13 18:39:31 -0400

    time test

--- TF_op/cmm_test.py
+++ TF_op/cmm_test.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python
 
 import tensorflow as tf
-import unittest
+import time
 
-#tf.compat.v1.disable_eager_execution()
 
-#classic_mm_module = tf.load_op_library('./classic_mat_mul.so')
 fast_mm_module = tf.load_op_library('./fast_mat_mul.so')
 
-#tf.random.set_seed(1)
+custom_time = 0
+regular_time = 0
 
+loops = 1000
+for i in range(loops):
 
+    a = tf.Variable(tf.random.uniform(shape=(1000, 1000)))
+    b = tf.Variable(tf.random.uniform(shape=(1000, 1000)))
 
-a = tf.Variable(tf.random.uniform(shape=(8000, 8000)))
-b = tf.Variable(tf.random.uniform(shape=(8000, 8000)))
+    t1 = time.time()
+    op = fast_mm_module.FastMatMul(a_matrix=a, b_matrix=b)
+    t2 = time.time()
 
-#op = classic_mm_module.ClassicMatMul(a_matrix=a, b_matrix=b)
-
-print("looping:")
-for i in range(10000):
-    #op = fast_mm_module.FastMatMul(a_matrix=a, b_matrix=b)
-
+    t3 = time.time()
     regular = tf.matmul(a, b)
-#
-# print('\n\n\nregular: ', regular)
-# print('\n\n\nop: ', op)
-# print('\n\n\nop-regular ', op-regular)
+    t4 = time.time()
 
+    custom_time += t2-t1
+    regular_time += t4-t3
 
-
+print(f'\n\nNumber of loops:{loops}')
+print(f'Average custom time: {custom_time/loops}')
+print(f'Average regular time: {regular_time/loops}')
