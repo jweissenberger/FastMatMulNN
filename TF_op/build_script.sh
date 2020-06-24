f0132939247abae721ce7d2c99aff444cf303ddd
Robin Zhang <zhanl317@wfu.edu>
2020-06-24 03:49:15 -0400

    update for more algorithms

--- TF_op/build_script.sh
+++ TF_op/build_script.sh
@@ -13,9 +13,20 @@ PAR_FLAGS="-fopenmp -D_PARALLEL_=1" # for DFS strategy (always call parallel MKL
 #PAR_FLAGS="-fopenmp -D_PARALLEL_=3" # for hybrid strategy (currently seg faults)
 
 # to save time, these env vars should be set by each user; 
-#TF_CFLAGS=($(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))'))
-#TF_LFLAGS=($(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))'))
+TF_CFLAGS=($(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))'))
+TF_LFLAGS=($(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))'))
 g++ -std=c++11 -shared classic_mat_mul.cc -o classic_mat_mul.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O2
 g++ -std=c++11 -shared dgemm_mat_mul.cc -o dgemm_mat_mul.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O3 $MKL_FLAGS
 g++ -fpermissive -std=c++11 -shared bini_mat_mul.cc -o bini_mat_mul.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O3 -I${FMMROOT}/linalg -I${FMMROOT}/util -I${FMMROOT}/algorithms $MKL_FLAGS
 g++ -fpermissive -std=c++11 -shared $PAR_FLAGS schonhage_mat_mul.cc -o schonhage_mat_mul.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O3 -I${FMMROOT}/linalg -I${FMMROOT}/util -I${FMMROOT}/algorithms $MKL_FLAGS
+g++ -fpermissive -std=c++11 -shared smirnov323_mat_mul.cc -o smirnov323_mat_mul.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O3 -I${FMMROOT}/linalg -I${FMMROOT}/util -I${FMMROOT}/algorithms $MKL_FLAGS
+g++ -fpermissive -std=c++11 -shared smirnov333_mat_mul.cc -o smirnov333_mat_mul.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O3 -I${FMMROOT}/linalg -I${FMMROOT}/util -I${FMMROOT}/algorithms $MKL_FLAGS
+g++ -fpermissive -std=c++11 -shared smirnov223_mat_mul.cc -o smirnov223_mat_mul.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O3 -I${FMMROOT}/linalg -I${FMMROOT}/util -I${FMMROOT}/algorithms $MKL_FLAGS
+g++ -fpermissive -std=c++11 -shared smirnov224_mat_mul.cc -o smirnov224_mat_mul.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O3 -I${FMMROOT}/linalg -I${FMMROOT}/util -I${FMMROOT}/algorithms $MKL_FLAGS
+g++ -fpermissive -std=c++11 -shared smirnov225_mat_mul.cc -o smirnov225_mat_mul.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O3 -I${FMMROOT}/linalg -I${FMMROOT}/util -I${FMMROOT}/algorithms $MKL_FLAGS
+#g++ -fpermissive -std=c++11 -shared smirnov272_mat_mul.cc -o smirnov272_mat_mul.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O3 -I${FMMROOT}/linalg -I${FMMROOT}/util -I${FMMROOT}/algorithms $MKL_FLAGS
+g++ -fpermissive -std=c++11 -shared smirnov334_mat_mul.cc -o smirnov334_mat_mul.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O3 -I${FMMROOT}/linalg -I${FMMROOT}/util -I${FMMROOT}/algorithms $MKL_FLAGS
+g++ -fpermissive -std=c++11 -shared smirnov442_mat_mul.cc -o smirnov442_mat_mul.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O3 -I${FMMROOT}/linalg -I${FMMROOT}/util -I${FMMROOT}/algorithms $MKL_FLAGS
+g++ -fpermissive -std=c++11 -shared smirnov444_mat_mul.cc -o smirnov444_mat_mul.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O3 -I${FMMROOT}/linalg -I${FMMROOT}/util -I${FMMROOT}/algorithms $MKL_FLAGS
+g++ -fpermissive -std=c++11 -shared smirnov552_mat_mul.cc -o smirnov552_mat_mul.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O3 -I${FMMROOT}/linalg -I${FMMROOT}/util -I${FMMROOT}/algorithms $MKL_FLAGS
+#g++ -fpermissive -std=c++11 -shared smirnov555_mat_mul.cc -o smirnov555_mat_mul.so -fPIC ${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O3 -I${FMMROOT}/linalg -I${FMMROOT}/util -I${FMMROOT}/algorithms $MKL_FLAGS
--- TF_op/multiply_time_test.py
+++ TF_op/multiply_time_test.py
@@ -11,15 +11,18 @@ mkl_get_max_threads = mkl_rt.MKL_Get_Max_Threads
 print( mkl_get_max_threads() )
 #mkl_set_num_threads(1)
 
-fast_mm_module = tf.load_op_library('./schonhage_mat_mul.so')
+algo_name = 'bini'
+epsilon_ = 1e-3
+step_ = 1
+fast_mm_module = tf.load_op_library('./%s_mat_mul.so'%algo_name)
 
 custom_time = 0
 regular_time = 0
 
 
 # to change TensorFlow's threads at runtime
-#tf.config.threading.set_intra_op_parallelism_threads(12)
-#tf.config.threading.set_inter_op_parallelism_threads(1)
+tf.config.threading.set_intra_op_parallelism_threads(12)
+tf.config.threading.set_inter_op_parallelism_threads(1)
 
 diff = 0
 
@@ -31,7 +34,7 @@ for i in range(loops):
     b = tf.Variable(tf.random.uniform(shape=(dim, dim)), dtype=tf.float32)
 
     t1 = time.time()
-    op = fast_mm_module.FastMatMul(a_matrix=b, b_matrix=a, epsilon=1e-2, steps=1)
+    op = fast_mm_module.FastMatMul(a_matrix=b, b_matrix=a, epsilon=epsilon_, steps=step_)
     t2 = time.time()
 
     t3 = time.time()
@@ -45,7 +48,11 @@ for i in range(loops):
 
 avg_custom = custom_time/loops
 avg_reg = regular_time/loops
-print(f'\n\nNumber of loops:{loops}')
+print(f'\n\nNumber of loops: {loops}')
+print(f'Matrix size: {dim}X{dim}')
+print(f'Epsilon: {epsilon_}')
+print(f'Steps: {step_}')
+print(f'Algorithm tested:{algo_name}')
 print(f'Average custom time: {avg_custom}')
 print(f'Average regular time: {avg_reg}')
 print(f'Times faster: {avg_reg/avg_custom}')
