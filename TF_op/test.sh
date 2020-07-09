f6af2237fa172e55ceb3d816b875b502bf0b6b18
Jack Weissenberger <johnaweissenberger@gmail.com>
2020-07-09 18:35:09 -0400

    add test.sh

--- TF_op/test.sh
+++ TF_op/test.sh
@@ -5,7 +5,7 @@ mkdir $outpath
 for name in regular bini_mat_mul
 do
 
-python custom_op_nn.py -u --layers 2 --nodes 30 --epochs 5 --bs 64 --mm ${name} \
+python -u custom_op_nn.py --layers 2 --nodes 30 --epochs 5 --bs 64 --mm ${name} \
                        > ${outpath}/test.log
 
 done
