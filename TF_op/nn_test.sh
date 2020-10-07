94cd4f85239402504d435b747748d9ca5bfdbd57
Jack Weissenberger <johnaweissenberger@gmail.com>
2020-10-07 08:12:09 -0700

    remove u arg from nn test

--- TF_op/nn_test.sh
+++ TF_op/nn_test.sh
@@ -11,7 +11,7 @@ do
 for bs in 64 128 256 512 1024 2048 4096 8192 16384
 do
 
-python custom_op_nn.py -u --layers ${layer} --nodes ${node} --epochs 5 --bs ${bs} --mm ${name} \
+python custom_op_nn.py --layers ${layer} --nodes ${node} --epochs 5 --bs ${bs} --mm ${name} \
                        > ${outpath}/layer_${layer}_nodes_${node}_bs_${bs}_mm_${name}.log
 
 done
