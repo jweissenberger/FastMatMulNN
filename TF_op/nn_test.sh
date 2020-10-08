ace9f92550d685fe8496a421598ee23254b4c16a
Jack Weissenberger <johnaweissenberger@gmail.com>
2020-10-07 17:09:58 -0700

    update nn test script

--- TF_op/nn_test.sh
+++ TF_op/nn_test.sh
@@ -2,13 +2,13 @@ outpath=./output_all_threads_fastmm_gradient_regular_bini_dgemm
 mkdir $outpath
 
 
+for layer in 4 8 16
+do
+for node in 256 1024 4096 8192
+do
+for bs in 256 1024 4096 8192
+do
 for name in regular bini322 dgemm
-do
-for layer in 4 16 32 64 128
-do
-for node in 256 512 1024 2048 4096 8192 16384
-do
-for bs in 64 128 256 512 1024 2048 4096 8192 16384
 do
 
 python custom_op_nn.py --layers ${layer} --nodes ${node} --epochs 5 --bs ${bs} --mm ${name} \
