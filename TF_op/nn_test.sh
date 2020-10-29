d0aa97100b08f0e382a27d981b2c2642b5c0be57
Jack Weissenberger <johnaweissenberger@gmail.com>
2020-10-28 18:06:40 -0700

    update nn_test

--- TF_op/nn_test.sh
+++ TF_op/nn_test.sh
@@ -2,13 +2,13 @@ outpath=./output_one_thread_fastmm_gradient_regular_bini_dgemm
 mkdir $outpath
 
 
-for size in 512 1024 4096 8192
+for size in 512 1024 2048 4096 8192
 do
 for name in regular bini322 dgemm
 do
 
 python custom_op_nn.py --layers 4 --nodes ${size} --epochs 5 --bs ${size} --mm ${name} \
-                       > ${outpath}/layer_${4}_nodes_${size}_bs_${size}_mm_${name}.log
+                       > ${outpath}/layer_4_nodes_${size}_bs_${size}_mm_${name}.log
 
 done
 done
