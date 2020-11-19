bbd79223040d7fc01d69b635e8ff3f7fda8285d4
Jack Weissenberger <johnaweissenberger@gmail.com>
2020-11-18 19:40:12 -0800

    update nn_test to test schonhage and all of the smirnov algorithms

--- TF_op/nn_test.sh
+++ TF_op/nn_test.sh
@@ -4,7 +4,7 @@ mkdir $outpath
 
 for size in 512 1024 2048 4096 8192
 do
-for name in bini322
+for name in schonhage333 smirnov224 smirnov225 smirnov272 smirnov323 smirnov333 smirnov334 smirnov442 smirnov444 smirnov552 smirnov555
 do
 
 python tensorboard_test.py --layers 4 --nodes ${size} --epochs 5 --bs ${size} --mm ${name} --logdir FIXEDoneThread_${name}${size}\
