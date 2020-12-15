f6e3e12d01619508ff8c9c1e13fc8c82f4c9aa73
Jack Weissenberger <johnaweissenberger@gmail.com>
2020-12-15 09:24:33 -0500

    add dgemm to tensorboard

--- TF_op/pull_tensorboard_data.py
+++ TF_op/pull_tensorboard_data.py
@@ -5,7 +5,7 @@ import json
 
 if __name__ == '__main__':
 
-    algorithm_names = ['bini322', 'schonhage333', 'smirnov224', 'smirnov225', 'smirnov272', 'smirnov323',
+    algorithm_names = ['dgemm', 'bini322', 'schonhage333', 'smirnov224', 'smirnov225', 'smirnov272', 'smirnov323',
                        'smirnov333', 'smirnov334', 'smirnov442', 'smirnov444', 'smirnov552', 'smirnov555'
                        ]
     matrix_sizes = [512, 1024, 2048, 4096, 8192]
