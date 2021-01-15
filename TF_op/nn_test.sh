814d258b1ab5bf3d5fc7448cff48fc0f21e67a67
Jack Weissenberger <johnaweissenberger@gmail.com>
2021-01-14 18:15:31 -0800

    update nn test.sh

--- TF_op/nn_test.sh
+++ TF_op/nn_test.sh
@@ -4,7 +4,7 @@ do
 for name in bini322 dgemm classic schonhage333 smirnov224 smirnov225 smirnov272 smirnov323 smirnov333 smirnov334 smirnov442 smirnov444 smirnov552 smirnov555
 do
 
-python tensorboard_test.py --layers 4 --nodes ${size} --epochs 5 --bs ${size} --mm ${name} --logdir all_threads_${name}${size}
+python tensorboard_test.py --layers 4 --nodes ${size} --epochs 5 --bs ${size} --mm ${name} --logdir FINAL_ONE_THREAD_${name}${size}
 
 done
 done
