7d15425bff3069af899aec45dd6c97c74036d3d0
Jack Weissenberger <johnaweissenberger@gmail.com>
2021-02-27 11:59:29 -0800

    update nn_test for 3 threads

--- TF_op/nn_test.sh
+++ TF_op/nn_test.sh
@@ -1,16 +1,14 @@
 
 for size in 512 1024 2048 4096 8192
 do
-#for name in bini322 dgemm classic schonhage333 smirnov224 smirnov225 smirnov272 smirnov323 smirnov333 smirnov334 smirnov442 smirnov444 smirnov552 smirnov555
-#do
-for thread in 1 12
+for name in bini322 dgemm classic schonhage333 smirnov224 smirnov225 smirnov272 smirnov323 smirnov333 smirnov334 smirnov442 smirnov444 smirnov552 smirnov555
 do
 
-python tensorboard_test.py --layers 4 --nodes ${size} --epochs 5 --bs ${size} --mm dgemm --logdir FINAL_${thread}_thread_dgemm${size} --num_threads ${thread}
+
+python tensorboard_test.py --layers 4 --nodes ${size} --epochs 5 --bs ${size} --mm ${name} --logdir PAPER_1_thread_${name}${size} --num_threads 1
 
 done
 done
-#done
 
 
 #python tensorboard_test.py --layers 4 --nodes 512 --epochs 5 --bs 512 --mm smirnov442 --logdir test
