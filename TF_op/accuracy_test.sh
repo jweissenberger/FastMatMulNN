16a0a1443e3abcf5a059573a975b56d1548acdd5
Jack Weissenberger <johnaweissenberger@gmail.com>
2021-03-03 13:07:03 -0800

    update for accuracy test

---
+++ TF_op/accuracy_test.sh
@@ -0,0 +1,8 @@
+
+for name in bini322 dgemm schonhage333 smirnov224 smirnov225 smirnov272 smirnov323 smirnov333 smirnov334 smirnov442 smirnov444 smirnov552 smirnov555
+do
+
+
+python tensorboard_test.py --layers 4 --nodes 300 --epochs 50 --bs 300 --mm ${name} --num_threads 12
+
+done
