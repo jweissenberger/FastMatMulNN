f141128c105f6134b16996be9f23866e8788b0a4
Jack Weissenberger <johnaweissenberger@gmail.com>
2021-03-03 13:09:22 -0800

    fix typo

--- TF_op/accuracy_test.sh
+++ TF_op/accuracy_test.sh
@@ -3,6 +3,6 @@ for name in bini322 dgemm schonhage333 smirnov224 smirnov225 smirnov272 smirnov3
 do
 
 
-python accuracy_test.py --layers 4 --nodes 300 --epochs 50 --bs 300 --mm ${name} --num_threads 12
+python accuracy_test.py --layers 2 --nodes 300 --epochs 50 --bs 300 --mm ${name} --num_threads 12
 
 done
